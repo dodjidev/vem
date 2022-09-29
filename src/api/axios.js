d5b17cc747f9154b7b89879eb2dbd9d0a5fdd1d4
dadev <dadevcontact@gmail.com>
2022-09-29 14:22:59 +0100

    baseUrl corrected

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const serverUrl = process.env.NODE_ENV == 'production' ? "https://vem-back.herokuapp.com" : http://localhost:3200'
+export const serverUrl = "https://vem-back.herokuapp.com"   // process.env.NODE_ENV == 'production' ?  : 'http://localhost:3200'
 
 export const _axios = axios.create({
     baseURL: serverUrl
