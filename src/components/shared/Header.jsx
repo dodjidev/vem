876080528cbd14c14185c3ee03634d984c2c8cb6
dadev <dadevcontact@gmail.com>
2022-10-07 16:50:39 +0100

    nav modified

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,16 +1,21 @@
 import { Button } from 'flowbite-react'
 import React from 'react'
 import { useState } from 'react'
-import { MenuOutline , CloseOutline} from 'react-ionicons'
+import { MenuOutline , CloseOutline } from 'react-ionicons'
 import Logo from '../../assets/icons/vem-logo.jpeg'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 import { getIsAdmin } from '../../features/user/user.slice'
 const Header = () => {
   let [navVisibleClass, setNavVisibleClass] = useState('hidden')
   const isAdmin = useSelector(getIsAdmin)
+  const navigate = useNavigate()
   const changeNavState = ()=> setNavVisibleClass(navVisibleClass => (navVisibleClass == 'visible' ? 'hidden' : 'visible'))
-  
+  const onNav = (path)=>{
+      setNavVisibleClass('hidden')
+      navigate(path)
+  }
   return (
     <div className='fixed w-full top-0 left-0 z-10'>
       <div className='md:flex bg-white  p-4 drop-shadow-lg items-center gaps-3 justify-between z-10'>
@@ -24,12 +29,12 @@ const Header = () => {
               </div>
              <div className={` md:flex  md:visible items-center justify-between w-[95%] ${navVisibleClass}`}>
                 <div className='md:ml-3 '>
-                    <a href="/#about-container" className='menu-link' >A propos</a>
-                    <Link to="/services" className='menu-link' >Nos services</Link>
-                    <Link to="/schools" className='menu-link' >Ecoles partenaires</Link>
-                    <Link to="/formations" className='menu-link' >Les formations</Link>
-                    <Link to="/degrees" className='menu-link' >Les diplomes</Link>
-                   { isAdmin && <Link to="/admin" className='menu-link' >Espace administration</Link>}
+                    <span onClick={() => onNav("/#about-container")} href="/#about-container" className='menu-link' >A propos</span>
+                    <span onClick={() => onNav("/services")}  className='menu-link' >Nos services</span>
+                    <span  onClick={() => onNav("/schools")}  to="/schools" className='menu-link' >Ecoles partenaires</span>
+                    <span  onClick={() => onNav("/formations")}  to="/formations" className='menu-link' >Les formations</span>
+                    <span  onClick={() => onNav("/degrees")}  to="/degrees" className='menu-link' >Les diplomes</span>
+                    { isAdmin && <span  onClick={() => onNav("/admin")}   to="/admin" className='menu-link' >Espace administration</span>}
                     {/* <a href="/#formations-container" className='menu-link' >Les formations</a>
                     <a href="/#diplomat-container" className='menu-link' >Les diplomes</a> */}
                 </div>
@@ -38,9 +43,7 @@ const Header = () => {
                       {/* <Button>
                         <a href="/#contact-container">CONTACTEZ-NOUS</a>
                       </Button> */}
-                      <Button>
-                        <Link to='/login'>Login</Link>
-                      </Button>
+                      <Button onClick={()=>onNav('/login')}>Connexion</Button>
                 </div>
              </div>
       </div>
